2472ef932ad323ff924dabb2f127169a4d030f02
bmeatayi <bmeatayi@gmail.com>
2018-12-29 14:42:02 +0100

    added print mode

--- src/explorepy/parser.py
+++ src/explorepy/parser.py
@@ -24,21 +24,32 @@ class Parser:
         data = self._convert(pid, bin_data)
         return pid
 
-    def _convert(self, pid, bin_data):
+    def _convert(self, pid, bin_data, mode='print'):
         if pid == 13:
             data = np.frombuffer(bin_data, dtype=self.dt_int16)
-            print("Accelerometer: ", data)
+            if mode == 'print':
+                print("Accelerometer: ", data)
+
         elif pid == 19:
-            temperature = np.frombuffer(bin_data[0], dtype=np.int8)
+            temperature = bin_data[0]   # np.frombuffer(bin_data[0], dtype=np.int8)
             light = (1000/4095) * np.frombuffer(bin_data[1:3], dtype=self.dt_uint16)
             battery = (16.8 / 6.8) * (1.8 / 2457) * np.frombuffer(bin_data[3:5], dtype=self.dt_uint16)
+            if mode == 'print':
+                print(f"Temperature: {temperature}, Light: {light}, Battery:{battery}")
+            data = (temperature, light, battery)
+
         elif pid == 27:
             pass  # TO DO: make sure the timestamp packet doesn't give an error
+
         elif pid == 144:
-            data = np.asarray([int.from_bytes(bin_data[x:x+3], byteorder='big', signed=True) for x in range(0, len(bin_data), 3)])
+            data = np.asarray([int.from_bytes(bin_data[x:x+3],
+                                              byteorder='little', signed=True) for x in range(0, len(bin_data), 3)])
             nChan = 5
             vref = 2.4
             nPacket = 33
             data = data.reshape((nChan, nPacket))
             eeg = data[1:, :].astype(np.float) * vref / (2 ^ 23 - 1) * 6/32
-            print("EEG data: ", eeg[:, 32])
+            if mode == 'print':
+                print("EEG data: ", eeg[:, 32])
+
+        return data
