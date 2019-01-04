2838c8ffd55b37121aec8d7f41419fa36c1cf1a9
bmeatayi <bmeatayi@gmail.com>
2019-01-04 18:13:45 +0100

    csv header was changed to 4 channel

--- src/explorepy/tools.py
+++ src/explorepy/tools.py
@@ -1,4 +1,3 @@
-import numpy as np
 from .parser import Parser
 import os.path
 import csv
@@ -14,7 +13,7 @@ def bin2csv(bin_file):
         parser = Parser(fid=f_bin)
         f_orn.write('TimeStamp, ax, ay, az, gx, gy, gz, mx, my, mz \n')
         f_orn.write('hh:mm:ss, mg/LSB, mg/LSB, mg/LSB, mdps/LSB, mdps/LSB, mdps/LSB, mgauss/LSB, mgauss/LSB, mgauss/LSB\n')
-        f_eeg.write('TimeStamp, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8\n')
+        f_eeg.write('TimeStamp, ch1, ch2, ch3, ch4\n')  # , ch5, ch6, ch7, ch8
         csv_eeg = csv.writer(f_eeg, delimiter=',')
         csv_orn = csv.writer(f_orn, delimiter=',')
         pid, timestamp, data = parser.parse_packet(mode='read')
