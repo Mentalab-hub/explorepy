f1346a94e8a8b176cf5cb3eb8264061e9d74ee11
bmeatayi <bmeatayi@gmail.com>
2019-01-04 18:13:04 +0100

    added recorder

--- src/explorepy/explore.py
+++ src/explorepy/explore.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .bt_client import BtClient
 from .parser import Parser
+import csv
 
 
 class Explore:
@@ -53,7 +54,7 @@ class Explore:
             self.device[device_id].is_connected = False
             self.disconnect(device_id)
 
-    def logdata(self):
+    def logData(self):
         r"""
         Print the data in the terminal/console
 
@@ -61,6 +62,37 @@ class Explore:
 
         """
         pass
+
+    def recordData(self, fileName, device_id=0):
+        exp_parser = Parser(socket=self.device[device_id].socket)
+
+        eeg_out_file = fileName + '_eeg.csv'
+        orn_out_file = fileName + '_orn.csv'
+
+        with open(eeg_out_file, "w") as f_eeg, open(orn_out_file, "w") as f_orn:
+            f_orn.write('TimeStamp, ax, ay, az, gx, gy, gz, mx, my, mz \n')
+            f_orn.write(
+                'hh:mm:ss, mg/LSB, mg/LSB, mg/LSB, mdps/LSB, mdps/LSB, mdps/LSB, mgauss/LSB, mgauss/LSB, mgauss/LSB\n')
+            f_eeg.write('TimeStamp, ch1, ch2, ch3, ch4\n')  # , ch5, ch6, ch7, ch8
+            csv_eeg = csv.writer(f_eeg, delimiter=',')
+            csv_orn = csv.writer(f_orn, delimiter=',')
+            pid, timestamp, data = exp_parser.parse_packet(mode='read')
+            while pid:
+                if pid == 144:
+                    csv_eeg.writerows(data.T.tolist())
+                if pid == 13:
+                    csv_orn.writerow([timestamp] + data.tolist())
+
+                pid, timestamp, data = exp_parser.parse_packet(mode='read')
+        try:
+            while True:
+                pid, timestamp, data = exp_parser.parse_packet(fileName=fileName, mode='record')
+                print("package ID: [%i]" % pid)
+        except ValueError:
+            # If value error happens, scan again for devices and try to reconnect (see reconnect function)
+            print("Disconnected, scanning for last connected device")
+            self.device[device_id].is_connected = False
+            self.disconnect(device_id)
 
     def push2lsl(self):
         r"""
