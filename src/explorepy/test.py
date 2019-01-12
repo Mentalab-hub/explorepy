e7db3e077728e440e071474886193fe5b65b2054
bmeatayi <bmeatayi@gmail.com>
2019-01-12 17:00:49 +0100

    cleaned unnecessary files

--- src/explorepy/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from explore import Explore
-f = open("demofile.txt", "w")
-
-explorer = Explore()
-
-explorer.connect(device_id = 0)
-explorer.recordData("testfile", device_id=0)
