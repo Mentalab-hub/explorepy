a5296a58635f89070516ae7aa95aac6218d884eb
ga25geh <ga25geh@mytum.de>
2019-01-18 13:16:32 +0100

    changes to bt_client and explorepy

--- src/explorepy/explore.py
+++ src/explorepy/explore.py
@@ -74,12 +74,21 @@ class Explore:
             f_orn.write('TimeStamp, ax, ay, az, gx, gy, gz, mx, my, mz \n')
             f_orn.write(
                 'hh:mm:ss, mg/LSB, mg/LSB, mg/LSB, mdps/LSB, mdps/LSB, mdps/LSB, mgauss/LSB, mgauss/LSB, mgauss/LSB\n')
-            f_eeg.write('TimeStamp, ch1, ch2, ch3, ch4\n')  # , ch5, ch6, ch7, ch8
+            f_eeg.write('TimeStamp, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8\n')
             csv_eeg = csv.writer(f_eeg, delimiter=',')
             csv_orn = csv.writer(f_orn, delimiter=',')
             pid, timestamp, data = exp_parser.parse_packet(mode='read')
             while pid:
                 if pid == 144:
+                   data[0, :] = timestamp
+                    csv_eeg.writerows(data.T.tolist())
+                if pid == 146:
+                   data[0, :] = timestamp
+                    csv_eeg.writerows([timestamp] + data.T.tolist())
+                if pid == 30:
+                    data[0, :] = timestamp
+                    csv_eeg.writerows(data.T.tolist())
+                if pid == 62:
                     csv_eeg.writerows(data.T.tolist())
                 if pid == 13:
                     csv_orn.writerow([timestamp] + data.tolist())
