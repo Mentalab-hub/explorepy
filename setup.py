53ec1782013db27e40b2db36499c18dae5385472
bmeatayi <bmeatayi@gmail.com>
2019-01-19 15:36:15 +0100

    pybluez version was removed

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        'numpy==1.15.4', 'pybluez>=0.21'
+        'numpy==1.15.4', 'pybluez'
     ],
     extras_require={
         # eg:
