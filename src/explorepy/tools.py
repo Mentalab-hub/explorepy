bd1c229430fe610eba3446dbbf8165ea16939a16
bmeatayi <bmeatayi@gmail.com>
2019-01-26 14:33:07 +0100

    clean up!

--- src/explorepy/tools.py
+++ src/explorepy/tools.py
@@ -11,22 +11,24 @@ def bin2csv(bin_file):
     c = 'y'
 
     if os.path.isfile(eeg_out_file):
-
-        c = input("Files with this name already exist, are you sure you want to proceed? [Enter y/n]")
-
-    if c == 'n':
-        exit()
-
-    with open(bin_file, "rb") as f_bin, open(eeg_out_file, "w") as f_eeg, open(orn_out_file, "w") as f_orn:
-        parser = Parser(fid=f_bin)
-        f_orn.write('TimeStamp, ax, ay, az, gx, gy, gz, mx, my, mz \n')
-        f_orn.write('hh:mm:ss, mg/LSB, mg/LSB, mg/LSB, mdps/LSB, mdps/LSB, mdps/LSB, mgauss/LSB, mgauss/LSB, mgauss/LSB\n')
-        f_eeg.write('TimeStamp, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8\n')
-        csv_eeg = csv.writer(f_eeg, delimiter=',')
-        csv_orn = csv.writer(f_orn, delimiter=',')
-        while True:
-            try:
-                packet = parser.parse_packet(mode='record', csv_files=(csv_eeg, csv_orn))
-            except ValueError:
-                print("Binary file ended suddenly!")
-                return 0
+        c = input("A file with this name already exist, are you sure you want to proceed? [Enter y/n]")
+    while True:
+        if c == 'n':
+            exit()
+        elif c == 'y':
+            with open(bin_file, "rb") as f_bin, open(eeg_out_file, "w") as f_eeg, open(orn_out_file, "w") as f_orn:
+                parser = Parser(fid=f_bin)
+                f_orn.write('TimeStamp, ax, ay, az, gx, gy, gz, mx, my, mz \n')
+                f_orn.write('hh:mm:ss, mg/LSB, mg/LSB, mg/LSB, mdps/LSB, mdps/LSB, mdps/LSB, mgauss/LSB, mgauss/LSB, mgauss/LSB\n')
+                f_eeg.write('TimeStamp, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8\n')
+                csv_eeg = csv.writer(f_eeg, delimiter=',')
+                csv_orn = csv.writer(f_orn, delimiter=',')
+                print("Converting...")
+                while True:
+                    try:
+                        packet = parser.parse_packet(mode='record', csv_files=(csv_eeg, csv_orn))
+                    except ValueError:
+                        print("Binary file ended suddenly! Conversion finished!")
+                        return 0
+        else:
+            c = input("A file with this name already exist, are you sure you want to proceed? [Enter y/n]")
