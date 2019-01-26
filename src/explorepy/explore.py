feec4619cfd8f45d9a0b18100c081690eeed2a66
bmeatayi <bmeatayi@gmail.com>
2019-01-26 14:42:01 +0100

    fixed minor bug in indentation

--- src/explorepy/explore.py
+++ src/explorepy/explore.py
@@ -86,29 +86,29 @@ class Explore:
         c = None
         if os.path.isfile(eeg_out_file):
             c = input("A file with this name already exist, are you sure you want to proceed? [Enter y/n]")
-        while True:
-            if c == 'n':
-                exit()
-            elif c == 'y':
-                with open(eeg_out_file, "w") as f_eeg, open(orn_out_file, "w") as f_orn:
-                    f_orn.write("TimeStamp, ax, ay, az, gx, gy, gz, mx, my, mz \n")
-                    f_orn.write(
-                        "hh:mm:ss, mg/LSB, mg/LSB, mg/LSB, mdps/LSB, mdps/LSB, mdps/LSB, mgauss/LSB, mgauss/LSB, mgauss/LSB\n")
-                    f_eeg.write("TimeStamp, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8\n")
-                    csv_eeg = csv.writer(f_eeg, delimiter=",")
-                    csv_orn = csv.writer(f_orn, delimiter=",")
+            while True:
+                if c == 'n':
+                    exit()
+                elif c == 'y':
+                    with open(eeg_out_file, "w") as f_eeg, open(orn_out_file, "w") as f_orn:
+                        f_orn.write("TimeStamp, ax, ay, az, gx, gy, gz, mx, my, mz \n")
+                        f_orn.write(
+                            "hh:mm:ss, mg/LSB, mg/LSB, mg/LSB, mdps/LSB, mdps/LSB, mdps/LSB, mgauss/LSB, mgauss/LSB, mgauss/LSB\n")
+                        f_eeg.write("TimeStamp, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8\n")
+                        csv_eeg = csv.writer(f_eeg, delimiter=",")
+                        csv_orn = csv.writer(f_orn, delimiter=",")
 
-                    is_acquiring = True
-                    print("Recording...")
-                    while is_acquiring:
-                        try:
-                            packet = self.parser.parse_packet(mode="record", csv_files=(csv_eeg, csv_orn))
-                        except ValueError:
-                            print("Disconnected, scanning for last connected device")
-                            self.device[device_id].is_connected = False
-                            is_acquiring = self.device[device_id].reconnect()
-            else:
-                c = input("A file with this name already exist, are you sure you want to proceed? [Enter y/n]")
+                        is_acquiring = True
+                        print("Recording...")
+                        while is_acquiring:
+                            try:
+                                packet = self.parser.parse_packet(mode="record", csv_files=(csv_eeg, csv_orn))
+                            except ValueError:
+                                print("Disconnected, scanning for last connected device")
+                                self.device[device_id].is_connected = False
+                                is_acquiring = self.device[device_id].reconnect()
+                else:
+                    c = input("A file with this name already exist, are you sure you want to proceed? [Enter y/n]")
 
     def push2lsl(self):
         r"""
