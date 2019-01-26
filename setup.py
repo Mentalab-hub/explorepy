1c254b05fba60df39ea77674bb531d1ec0f1f22a
bmeatayi <bmeatayi@gmail.com>
2019-01-26 16:10:31 +0100

    removed python2

--- setup.py
+++ setup.py
@@ -63,9 +63,7 @@ setup(
         'numpy', 'pybluez'  # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
+
     },
     entry_points={
         'console_scripts': [
