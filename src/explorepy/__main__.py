91b8d2d6bd2ae1f58d0b5ea43bf0c96cf6a3048e
ga25geh <ga25geh@mytum.de>
2019-02-22 14:57:03 +0100

    CLI WIP

--- src/explorepy/__main__.py
+++ src/explorepy/__main__.py
@@ -9,8 +9,19 @@ Why does this file exist, and why __main__? For more info, read:
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
 import sys
+import argparse
+from explorepy.cli import CLI
 
-from explorepy.cli import main
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Python package for the Mentalab Explore',
+        usage="Usage")
+
+    args = parser.parse_args(sys.argv)
+
+    cli = CLI(args.command)
+
 
 if __name__ == "__main__":
     sys.exit(main())
