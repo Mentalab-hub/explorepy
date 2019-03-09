abe96863c70c7af7a64b2be350517226ef2eb783
philippjak <philippjak@googlemail.com>
2019-03-09 15:43:13 +0100

    Implementation of a butterworth filter, possibility of changing settings

---
+++ src/explorepy/filters.py
@@ -0,0 +1,32 @@
+import numpy
+import scipy
+
+
+class Filter:
+    def __init__(self):
+        self.cutoffA = 0.5
+        self.cutoffB = 40
+        self.sample_frequency = 1000.0
+        self.order = 5
+
+    def set_bandpass(self, a, b, fs, order):
+        self.cutoffA = a
+        self.cutoffB = b
+        self.sample_frequency = fs
+        self.order = order
+
+    def butter_bandpass(self):
+        nyq = 0.5*self.sample_frequency
+        low_a = self.cutoffA/nyq
+        high_b = self.cutoffB/nyq
+        b, a = butter(order, [low_a, high_b], btype='band')
+        return b, a
+
+    def apply_band(self, data):
+        self.is_not_used()
+        b, a = butter_bandpass()
+        filt_data = lfilter(b, a, data)
+        return filt_data
+
+    def is_not_used(self):
+        pass
