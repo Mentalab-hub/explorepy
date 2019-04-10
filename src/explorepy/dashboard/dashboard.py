1e154a3052949654a5ecc68c902f40f63e5ef394
bmeatayi <bmeatayi@gmail.com>
2019-04-10 11:05:25 +0200

    changed layout and added vertical line at the end of plot

--- src/explorepy/dashboard/dashboard.py
+++ src/explorepy/dashboard/dashboard.py
@@ -4,7 +4,7 @@ from functools import partial
 from threading import Thread
 import os.path
 
-from bokeh.layouts import column, layout
+from bokeh.layouts import column, layout, widgetbox, row
 from bokeh.models import ColumnDataSource, ResetTool, PrintfTickFormatter
 from bokeh.plotting import figure
 from bokeh.server.server import Server
@@ -18,7 +18,7 @@ EEG_SRATE = 250  # Hz
 WIN_LENGTH = 10  # Seconds
 CHAN_LIST = ['Ch1', 'Ch2', 'Ch3', 'Ch4', 'Ch5', 'Ch6', 'Ch7', 'Ch8']
 DEFAULT_SCALE = 10 ** -3  # Volt
-SCALE_MENU = {"1 uV": 6., "5 uV": 5.3333, "10 uV": 5., "100 uV": 4., "500 uV": 3.3333, "1 mV": 3.,
+SCALE_MENU = {"1 uV": 6., "5 uV": 5.3333, "10 uV": 5., "100 uV": 4., "500 uV": 3.3333, "1 mV": 3., "5 mV": 2.3333,
               "10 mV": 2., "100 mV": 1.}
 TIME_RANGE_MENU = {"10 s": 10., "5 s": 5., "20 s": 20.}
 
@@ -60,7 +60,7 @@ class Dashboard:
         # Create plot
         self.plot = figure(y_range=(0.01, self.n_chan + 1 - 0.01), y_axis_label='Voltage', x_axis_label='Time (s)',
                            title="EEG signal",
-                           plot_height=600, plot_width=1200,
+                           plot_height=600, plot_width=1270,
                            y_minor_ticks=int(10),
                            tools=[ResetTool()], active_scroll=None, active_drag=None,
                            active_inspect=None, active_tap=None)
@@ -68,41 +68,50 @@ class Dashboard:
         # Set yaxis properties
         self.plot.yaxis.ticker = SingleIntervalTicker(interval=1, num_minor_ticks=10)
 
+        # Initial plot line
         for i in range(self.n_chan):
             self.plot.line(x='t', y=CHAN_LIST[i], source=self.exg_source,
                            line_width=1.5)
+
+        # Initial vertical line
+        self.plot.line(x=[0, 0],
+                       y=[self.offsets[-1]-2, self.offsets[0]+2],
+                       name='vertical_line',
+                       line_width=2,
+                       color='red', alpha=.8)
+
         self.plot.x_range.follow = "end"
         self.plot.x_range.follow_interval = WIN_LENGTH
 
         # Create controls
-        self.t_range = Select(title="Time Range", value="10 s", options=list(TIME_RANGE_MENU.keys()))
+        self.t_range = Select(title="Time Range", value="10 s", options=list(TIME_RANGE_MENU.keys()), width=210)
         self.t_range.on_change('value', self._change_t_range)
-        self.y_scale = Select(title="Y-axis Scale", value="1 mV", options=list(SCALE_MENU.keys()))
+        self.y_scale = Select(title="Y-axis Scale", value="1 mV", options=list(SCALE_MENU.keys()), width=210)
         self.y_scale.on_change('value', self._change_scale)
 
         # Create device info tables
         columns = [TableColumn(field='firmware_version', title="Firmware Version")]
         self.firmware = DataTable(source=self.firmware_source, index_position=None, sortable=False, reorderable=False,
-                                  columns=columns, width=270, height=50)
+                                  columns=columns, width=200, height=50)
 
         columns = [TableColumn(field='battery', title="Battery")]
         self.battery = DataTable(source=self.battery_source, index_position=None, sortable=False, reorderable=False,
-                                 columns=columns, width=270, height=50)
+                                 columns=columns, width=200, height=50)
 
         columns = [TableColumn(field='temperature', title="temperature")]
-        self.temperature = DataTable(source=self.temperature_source, index_position=None, sortable=False, reorderable=False,
-                                     columns=columns, width=270, height=50)
+        self.temperature = DataTable(source=self.temperature_source, index_position=None, sortable=False,
+                                     reorderable=False, columns=columns, width=200, height=50)
 
         columns = [TableColumn(field='light', title="light")]
         self.light = DataTable(source=self.light_source, index_position=None, sortable=False, reorderable=False,
-                               columns=columns, width=270, height=50)
+                               columns=columns, width=200, height=50)
 
         # Add widgets to the doc
-        m_layout = layout([[column(self.y_scale, self.t_range, self.firmware,
-                                   self.battery, self.temperature, self.light),
-                            self.plot]], sizing_mode="fixed")
+        m_widgetbox = widgetbox([self.y_scale, self.t_range, self.firmware,
+                                 self.battery, self.temperature, self.light],
+                                width=220)
 
-        self.doc.add_root(m_layout)
+        self.doc.add_root(row([m_widgetbox, self.plot]))
 
         # Set the theme
         module_path = os.path.dirname(__file__)
@@ -122,10 +131,23 @@ class Dashboard:
             ExG (np.ndarray): array of new data
 
         """
+        # Delete old vertical line
+        vertical_line = self.plot.select_one({'name': 'vertical_line'})
+        self.plot.renderers.remove(vertical_line)
+
+        # Update vertical line
+        self.plot.line(x=[time_vector[-1], time_vector[-1]],
+                       y=[self.offsets[-1] - 2, self.offsets[0] + 2],
+                       name='vertical_line',
+                       line_width=2,
+                       color='red', alpha=.8)
+
+        # Update ExG data
         ExG = self.offsets + ExG / self.y_unit
         new_data = dict(zip(self.chan_key_list, ExG))
         new_data['t'] = time_vector
         self.exg_source.stream(new_data, rollover=2 * EEG_SRATE * WIN_LENGTH)
+
 
     @gen.coroutine
     def update_info(self, new):
