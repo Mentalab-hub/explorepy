a823c642b0c10ed7a8e7933ae17098b919980447
bmeatayi <bmeatayi@gmail.com>
2019-04-25 12:18:34 +0200

    add notch filter

--- src/explorepy/filters.py
+++ src/explorepy/filters.py
@@ -1,14 +1,16 @@
 import numpy as np
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, lfilter, iirnotch, iirfilter
 
 
 class Filter:
-    def __init__(self, l_freq, h_freq):
+    def __init__(self, l_freq, h_freq, line_freq=50):
         self.low_cutoff_freq = l_freq
         self.high_cutoff_freq = h_freq
+        self.line_freq = line_freq
         self.sample_frequency = 250.0
         self.order = 5
-        self.param = None
+        self.bp_param = None
+        self.notch_param = None
 
     def _design_filter(self, nchan):
         nyq = 0.5 * self.sample_frequency
@@ -16,14 +18,32 @@ class Filter:
         high_freq = self.high_cutoff_freq / nyq
         b, a = butter(self.order, [low_freq, high_freq], btype='band')
         zi = np.zeros((nchan, 10))  # lfiltic(b, a, (0.,))
-        self.param = {'a': a, 'b': b, 'zi': zi}
+        self.bp_param = {'a': a, 'b': b, 'zi': zi}
+
+    def _design_notch_filter(self, nchan):
+        # Q = 50
+        # b, a = iirnotch(self.line_freq, Q, self.sample_frequency)
+        nyq = 0.5 * self.sample_frequency
+        low_freq = (self.line_freq-2) / nyq
+        high_freq = (self.line_freq+2) / nyq
+        b, a = iirfilter(5, [low_freq, high_freq], btype='bandstop', ftype='butter')
+        zi = np.zeros((nchan, 10))
+        self.notch_param = {'a': a, 'b': b, 'zi': zi}
 
     def apply_bp_filter(self, raw_data):
-        if self.param is None:
+        if self.bp_param is None:
             self._design_filter(nchan=raw_data.shape[0])
 
-        filtered_data, zi = lfilter(self.param['b'], self.param['a'], raw_data, zi=self.param['zi'])
-        self.param['zi'] = zi
+        filtered_data, zi = lfilter(self.bp_param['b'], self.bp_param['a'], raw_data, zi=self.bp_param['zi'])
+        self.bp_param['zi'] = zi
+        return filtered_data
+
+    def apply_notch_filter(self, raw_data):
+        if self.notch_param is None:
+            self._design_notch_filter(nchan=raw_data.shape[0])
+
+        filtered_data, zi = lfilter(self.notch_param['b'], self.notch_param['a'], raw_data, zi=self.notch_param['zi'])
+        self.notch_param['zi'] = zi
         return filtered_data
 
 
@@ -35,15 +55,21 @@ if __name__ == '__main__':
     omega = 2 * np.pi * 25
     x = 0.5 * np.sin(omega * t)
     x = np.repeat(x[np.newaxis,:], 4, axis=0)
-    x_noisy = x + .4 * np.random.rand(4, n_sample) + .4  # np.cos(500 * np.pi * t) + .5
+    x_noisy = x + .2 * np.random.rand(4, n_sample) + np.cos(2 * np.pi * 50 * t) + .5
 
-    filt = Filter(l_freq=20, h_freq=30)
-    x_filt = filt.apply_bp_filter(x_noisy)
+    filt = Filter(l_freq=20, h_freq=30, line_freq=50)
+    x_filt = filt.apply_notch_filter(filt.apply_bp_filter(x_noisy))
 
     # test real-time filtering
     x_filt_realtime = np.zeros((4, 0))
     for i in range(n_chunk):
-        x_filt_realtime = np.concatenate((x_filt_realtime, filt.apply_bp_filter(x_noisy[:, i * 33:(i + 1) * 33])), axis=1)
+        x_filt_realtime = np.concatenate((x_filt_realtime,
+                                          filt.apply_notch_filter(filt.apply_bp_filter(x_noisy[:, i * 33:(i + 1) * 33])))
+                                         , axis=1)
+        # Only notch filter
+        # x_filt_realtime = np.concatenate((x_filt_realtime,
+        #                                   filt.apply_notch_filter(x_noisy[:, i * 33:(i + 1) * 33]))
+        #                                  , axis=1)
 
     plt.figure(1)
     plt.clf()
