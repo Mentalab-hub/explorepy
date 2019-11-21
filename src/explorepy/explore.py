a40c612294c64c9d972384d57aa20e9408b015ec
Masoome Fazelian <fazelian.masoome@gmail.com>
2019-11-21 11:11:19 +0100

    merge bug

--- src/explorepy/explore.py
+++ src/explorepy/explore.py
@@ -10,7 +10,7 @@ import time
 from pylsl import StreamInfo, StreamOutlet
 from threading import Thread, Timer
 from datetime import datetime
-from explorepy.packet import CommandRCV, CommandStatus
+from explorepy.packet import CommandRCV, CommandStatus, CalibrationInfo, MarkerEvent
 
 
 class Explore:
@@ -265,90 +265,6 @@ class Explore:
             imp_deactivate_cmd = command.ZmeasurementDisable()
             self.change_settings(imp_deactivate_cmd)
 
-    def pass_msg(self, device_id=0, msg2send=None):
-        r"""
-        sends a set of parameters to the device
-        Returns:
-        sample commands and messages:
-        msg = Host time stamp: default message if the msg2send field is empty
-        msg = b'\xA0\x00\x0A\x00\xda\xba\xad\xde\xA1\x02\xaf\xbe\xad\xde' it is the command to switch to 500sps mode
-        msg = b'\xA0\x00\x0A\x00\xda\xba\xad\xde\xA3\x00\xaf\xbe\xad\xde' it is the command to format memory
-
-        example:
-        myexplore.pass_msg(msg2send=command.Command.FORMAT_MEMORY.value)
-        """
-
-        if self.socket is None:
-            self.socket = self.device[device_id].bt_connect()
-
-        if self.parser is None:
-            self.parser = Parser(socket=self.socket)
-
-        if msg2send is None:
-            # current date and time
-            msg_is_command = 0
-            now = datetime.now()
-            print(now)
-            timestamp = int(1000000000 * datetime.timestamp(now))  # time stamp in nanosecond
-            ts_str = hex(timestamp)
-            ts_str = ts_str[2:18]
-            host_ts = bytes.fromhex(ts_str)
-            ID = b'\x1B'
-            CNT = b'\x01'
-            Payload = b'\x10\x00' # i.e. 0x0010
-            device_ts = b'\x00\x00\x00\x00'
-            Fletcher = b'\xFF\xFF\xFF\xFF'
-            msg2send = ID + CNT + Payload + device_ts + host_ts + Fletcher
-        else:
-            msg_is_command = msg2send[-6]
-        is_sending = True
-
-        while is_sending:
-            try:
-                time.sleep(0.1)
-                self.parser.send_msg(msg2send)
-                print(" Message Sent :)")
-                is_sending = False
-            except ValueError:
-                # If value error happens, scan again for devices and try to reconnect (see reconnect function)
-                print("Disconnected, scanning for last connected device")
-                socket = self.device[device_id].bt_connect()
-                self.parser.socket = socket
-            except bluetooth.BluetoothError as error:
-                print("Bluetooth Error: attempting reconnect. Error: ", error)
-                self.parser.socket = self.device[device_id].bt_connect()
-
-        is_listening = [True]
-        command_processed = False
-
-        def stop_listening(flag):
-            flag[0] = False
-
-        Timer(100, stop_listening, [is_listening]).start()
-        print("waiting for ack and status messages...")
-        while is_listening[0]:
-            try:
-                packet = self.parser.parse_packet(mode="listen")
-                if isinstance(packet, CommandRCV):
-                    if packet.opcode == msg_is_command:
-                        print ("the opcode matches the sent command, Explore has received the command")
-                if isinstance(packet, CommandStatus):
-                    if packet.opcode == msg_is_command:
-                        print ("the opcode matches the sent command, Explore has processed the command")
-                        is_listening = [False]
-                        command_processed = True
-
-            except ValueError:
-                # If value error happens, scan again for devices and try to reconnect (see reconnect function)
-                print("Disconnected, scanning for last connected device")
-                socket = self.device[device_id].bt_connect()
-                self.parser.socket = socket
-            except bluetooth.BluetoothError as error:
-                print("Bluetooth Error: attempting reconnect. Error: ", error)
-                self.parser.socket = self.device[device_id].bt_connect()
-        if not command_processed:
-            print("No status message has been received after ", 100, " seconds. Please send the command again")
-
     def change_settings(self, command, device_id=0):
         """
         sends a message to the device
@@ -394,6 +310,9 @@ class Explore:
                     temp = command.int2bytearray(packet.opcode, 1)
                     if command.int2bytearray(packet.opcode, 1) == command.opcode.value:
                         print("The opcode matches the sent command, Explore has received the command")
+                if isinstance(packet, CalibrationInfo):
+                    self.parser.imp_calib_info['slope'] = packet.slope
+                    self.parser.imp_calib_info['offset'] = packet.offset
                 if isinstance(packet, CommandStatus):
                     if command.int2bytearray(packet.opcode,1) == command.opcode.value:
                         print("The opcode matches the sent command, Explore has processed the command")
