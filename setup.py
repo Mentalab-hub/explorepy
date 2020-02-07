ffa723715c89c1940894520d1208a99868f20362
bmeatayi <bmeatayi@gmail.com>
2020-02-07 10:44:29 +0100

    add pyEDFlib to dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
         return fh.read()
 
 
-my_req = ['numpy', 'scipy']
+my_req = ['numpy', 'scipy', 'pyedflib==0.1.15']
 if not os.environ.get('READTHEDOCS'):
     my_req.append('pybluez==0.22')  # Add pybluez if the environment is other than READTHEDOCS
     my_req.append('pylsl')
