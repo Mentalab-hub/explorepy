45dcedf94df964ab9c4e36824f6b12b692df9e65
bmeatayi <bmeatayi@gmail.com>
2020-02-28 09:29:19 +0100

    add stream processor

---
+++ src/explorepy/stream_processor.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from explorepy.parser import Parser
+
+
+class StreamProcessor:
+    def __init__(self):
+        self.parser = None
+        self.filters = []
+        self.orn_calibrator = None
+
+    def start(self, device_name=None, mac_address=None):
+        self.parser = Parser(stream_processor=self, mode='device')
+        self.parser.start_stream(device_name, mac_address)
+
+    def process(self, packet):
+        print(packet)
+
+    def apply_filters(self):
+        pass
