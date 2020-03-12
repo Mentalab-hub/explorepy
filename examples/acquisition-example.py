ffbc772b881356c62c847251cb9f1647d0fc137a
bmeatayi <bmeatayi@gmail.com>
2020-03-12 13:51:43 +0100

    adapt example to the new design

--- examples/acquisition-example.py
+++ examples/acquisition-example.py
@@ -1,13 +1,27 @@
 """An example code for data acquisition from Explore device"""
 
+import time
 import explorepy
+from explorepy.stream_processor import TOPICS
 import argparse
 
 
-def streaming(parser):
-    """A generator function for parsed packets"""
-    while True:
-        yield parser.parse_packet()
+def my_exg_function(packet):
+    """A function that receives ExG packets and does some operations on the data"""
+    t_vector, exg_data = packet.get_data()
+    print("Received an ExG packet with data shape: ", exg_data.shape)
+    #############
+    # YOUR CODE #
+    #############
+
+
+def my_orn_function(packet):
+    """A function that receives orientation packets and does some operations on the data"""
+    timestamp, orn_data = packet.get_data()
+    print("Received an orientation packet with data shape: ", orn_data.shape)
+    #############
+    # YOUR CODE #
+    #############
 
 
 def main():
@@ -21,12 +35,14 @@ def main():
     # Connect to the Explore device using device bluetooth name or mac address
     exp_device.connect(device_name=args.name)
 
-    # Instantiate a generator object
-    gen_func = streaming(exp_device.parser)
-
-    for packet in gen_func:
-        print(packet)
-        # YOUR CODE
+    # Subscribe your function to the stream publisher
+    exp_device.stream_processor.subscribe(callback=my_exg_function, topic=TOPICS.raw_ExG)
+    exp_device.stream_processor.subscribe(callback=my_orn_function, topic=TOPICS.raw_orn)
+    try:
+        while True:
+            time.sleep(.5)
+    except KeyboardInterrupt:
+        return
 
 
 if __name__ == "__main__":
