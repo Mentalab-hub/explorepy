8f67e9bb4ddc9533376bb84c7b9455ecb4a4329f
salman2135 <salman.2135@gmail.com>
2020-04-24 10:54:50 +0200

    added with exploresdk files

---
+++ lib/BluetoothHelpers.cpp
@@ -0,0 +1,63 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "bthprops.lib")
+#include <winsock2.h>
+#include <ws2bth.h>
+#include <string>
+#include<locale>
+#include<codecvt>
+#include "BluetoothHelpers.h"
+
+using namespace std;
+
+bool BluetoothHelpers::Initialize()
+{
+	WSADATA data;
+	int startupError = WSAStartup(MAKEWORD(2, 2), &data);
+	bool initializationSuccess = startupError == 0;
+
+	if (initializationSuccess)
+	{
+		initializationSuccess = LOBYTE(data.wVersion) == 2 && HIBYTE(data.wVersion) == 2;
+
+		if (!initializationSuccess)
+			BluetoothHelpers::Finalize();
+	}
+
+	return initializationSuccess;
+}
+
+void BluetoothHelpers::Finalize()
+{
+	WSACleanup();
+}
+
+string BluetoothHelpers::GetWSAErrorMessage(int errorCode)
+{
+	LPTSTR buffer;
+
+	if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, errorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&buffer, 0, NULL) == 0)
+	{
+		return "Unknown";
+	}
+
+	char buff[20];
+	auto result = ToString(buffer);
+	LocalFree(buffer);
+	_itoa_s(errorCode, buff, sizeof(buff), 10);
+	return string("(") + buff + string(") ") + result;
+}
+
+string BluetoothHelpers::ToString(LPTSTR str)
+{
+#ifdef UNICODE
+	std::wstring_convert<codecvt_utf8<wchar_t>, wchar_t> converter;
+	return converter.to_bytes(wstring(str));
+#else
+	return string(str);
+#endif
+}
