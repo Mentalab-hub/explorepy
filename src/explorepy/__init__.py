9c36d0c56f61a2c2eafb3a2fecb585cfa7b59e7d
salman2135 <salman.2135@gmail.com>
2020-04-29 13:02:26 +0200

    added .pyd copy

--- src/explorepy/__init__.py
+++ src/explorepy/__init__.py
@@ -1,8 +1,21 @@
+import os
+import shutil
+
+parent_directory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
+files = (file for file in os.listdir(parent_directory)
+         if os.path.isfile(os.path.join(parent_directory, file)))
+
+for file in files:
+    if '.pyd' in file:
+        full_path = os.path.join(parent_directory, file)
+        shutil.copy(full_path, os.getcwd())
+
 from .explore import Explore
 from . import tools, command
+
 __version__ = '0.6.0'
 _bt_interface = 'sdk'
-
 
 def set_bt_interface(bt_interface):
     """Set Explorepy Bluetooth interface type
@@ -15,4 +28,11 @@ def set_bt_interface(bt_interface):
         raise ValueError
     import explorepy
     explorepy._bt_interface = bt_interface
+
+
+
+
+
+
+
 
