020710c7e1c9946058aa701fdb3b525dfd5d0813
Atayi <35002731+bmeatayi@users.noreply.github.com>
2020-05-20 13:36:52 +0200

    exclude exploresdk in RTD env (#86)

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ from os.path import dirname
 from os.path import join
 from os.path import splitext
 
-
 from setuptools import find_packages
 from setuptools import setup
 from setuptools import Extension
 from setuptools.command.develop import develop
 from setuptools.command.install import install
+
 
 def read(*names, **kwargs):
     with io.open(
@@ -29,41 +29,39 @@ def read(*names, **kwargs):
 
 
 my_req = ['numpy', 'scipy', 'pyedflib==0.1.15', 'click==7.0', 'appdirs==1.4.3']
+ext_modules_list = []
 if not os.environ.get('READTHEDOCS'):
     my_req.append('pybluez==0.22')  # Add pybluez if the environment is other than READTHEDOCS
     my_req.append('pylsl')
     my_req.append('bokeh==1.4.0')
 
-libPath = "lib"
-current_platform = sys.platform
-if current_platform == 'win32' or current_platform == 'win64':
-    windows_lib_path = os.path.join(libPath, 'windows')
-    
-    moduleExploresdk = Extension(
-        name='_exploresdk',
-        sources=[os.path.join(windows_lib_path, 'swig_interface_wrap.cxx'),
-                os.path.join(windows_lib_path, 'BluetoothHelpers.cpp'),
-                os.path.join(windows_lib_path, 'DeviceINQ.cpp'),
-                os.path.join(windows_lib_path, 'BTSerialPortBinding.cpp')],
-        swig_opts=['-c++']
-    )
+    libPath = "lib"
+    current_platform = sys.platform
+    if current_platform == 'win32' or current_platform == 'win64':
+        windows_lib_path = os.path.join(libPath, 'windows')
+        ext_modules_list.append(Extension(
+            name='_exploresdk',
+            sources=[os.path.join(windows_lib_path, 'swig_interface_wrap.cxx'),
+                     os.path.join(windows_lib_path, 'BluetoothHelpers.cpp'),
+                     os.path.join(windows_lib_path, 'DeviceINQ.cpp'),
+                     os.path.join(windows_lib_path, 'BTSerialPortBinding.cpp')],
+            swig_opts=['-c++']
+        ))
 
-elif current_platform.startswith('linux'):
-    linux_lib_path = os.path.join(libPath, 'linux')
+    elif current_platform.startswith('linux'):
+        linux_lib_path = os.path.join(libPath, 'linux')
 
-    moduleExploresdk = Extension(
-        name='_exploresdk',
-        sources=[os.path.join(linux_lib_path, 'swig_interface_wrap.cxx'),
-                os.path.join(linux_lib_path, 'DeviceINQ.cpp'),
-                os.path.join(linux_lib_path, 'BTSerialPortBinding.cpp')],
-	extra_link_args=["-lbluetooth"],
-        swig_opts=['-c++']
-    )
-else:
-    ##Mac implementation
-    source_files = []
-
-
+        ext_modules_list.append(Extension(
+            name='_exploresdk',
+            sources=[os.path.join(linux_lib_path, 'swig_interface_wrap.cxx'),
+                     os.path.join(linux_lib_path, 'DeviceINQ.cpp'),
+                     os.path.join(linux_lib_path, 'BTSerialPortBinding.cpp')],
+            extra_link_args=["-lbluetooth"],
+            swig_opts=['-c++']
+        ))
+    else:
+        # Mac implementation
+        source_files = []
 
 setup(
     name='explorepy',
@@ -81,7 +79,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    ext_modules=[moduleExploresdk],
+    ext_modules=ext_modules_list,
     include_package_data=True,
     zip_safe=False,
     classifiers=[
