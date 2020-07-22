6b16269609b2526487085c9e28182adf441c337d
salman rahman <salmanrahman@salmans-MacBook-Pro.local>
2020-07-22 17:45:57 +0200

    added setup.py that can create wheel

--- setup.py
+++ setup.py
@@ -97,8 +97,8 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    data_files=[('lib', ['lib/_exploresdk.so'])],
     ext_modules=ext_modules_list,
-    package_data={'': ['exploresdk.so']},
     include_package_data=True,
     zip_safe=False,
     classifiers=[
