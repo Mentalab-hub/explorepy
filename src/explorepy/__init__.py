152efa0b1df38a9a7fc34fd75a5d561ea00d4d8d
salman rahman <salman2135@gmail.com>
2020-11-06 15:02:24 +0100

    added fix for library path, by changing library namespace

--- src/explorepy/__init__.py
+++ src/explorepy/__init__.py
@@ -2,24 +2,6 @@ import os
 import shutil
 from sys import platform as _platform
 
-parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-
-files = (file for file in os.listdir(parent_directory)
-         if os.path.isfile(os.path.join(parent_directory, file)))
-
-if _platform == "linux" or _platform == "linux2":
-    for file in files:
-        if '_exploresdk' in file:
-            full_path = os.path.join(parent_directory, file)
-            shutil.copy(full_path, os.path.dirname(__file__))
-
-elif _platform == "win32" or _platform == "win64":
-    for file in files:
-        if '_exploresdk' in file:
-            full_path = os.path.join(parent_directory, file)
-            shutil.copy(full_path, os.path.dirname(__file__))
-
-
 from .explore import Explore
 from . import tools, command, exploresdk
 from explorepy.dashboard.dashboard import Dashboard
