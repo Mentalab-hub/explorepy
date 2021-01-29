18e92bc3d31f49613c7777be55a9b2159b312a12
bmeatayi <bmeatayi@gmail.com>
2021-01-29 15:50:24 +0100

    add logger

--- src/explorepy/__init__.py
+++ src/explorepy/__init__.py
@@ -1,9 +1,24 @@
+import logging
+import platform
+import sys
+from . import log_config
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.debug("NEW SESSION ---------------------------------------------")
+logger.debug("Explorepy starting...")
+logger.debug(f"OS: {platform.platform()} - {sys.version}")
+
 from .explore import Explore
 from . import tools, command, exploresdk
 from explorepy.dashboard.dashboard import Dashboard
 
+
 __version__ = '1.3.0'
 _bt_interface = 'pybluez'
+if not sys.version_info >= (3, 6):
+    raise EnvironmentError("Explorepy requires python versions 3.6 or newer!")
+logger.debug(f"Explorepy version: {__version__}")
+logger.debug(f"BT interface: {_bt_interface}")
 
 
 def set_bt_interface(bt_interface):
@@ -14,6 +29,7 @@ def set_bt_interface(bt_interface):
 
     """
     if bt_interface not in ['sdk', 'pybluez']:
-        raise ValueError
+        raise ValueError("bt_interface must be either sdk or pybluez")
     import explorepy
     explorepy._bt_interface = bt_interface
+    logger.info(f"BT interface was changed to {bt_interface}")
