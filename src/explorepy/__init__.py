b9b8f6f30c1ae6567dc34bfdda9200206be39fbc
bmeatayi <bmeatayi@gmail.com>
2021-02-18 14:16:48 +0100

    add sentry

--- src/explorepy/__init__.py
+++ src/explorepy/__init__.py
@@ -5,9 +5,8 @@ from . import log_config
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.debug("NEW SESSION ---------------------------------------------")
-logger.debug("Explorepy starting...")
 logger.debug(f"OS: {platform.platform()} - {sys.version}")
-
+log_config.log_breadcrumb(f"OS: {platform.platform()} - {sys.version}", "info")
 from .explore import Explore
 from . import tools, command, exploresdk
 from explorepy.dashboard.dashboard import Dashboard
@@ -15,10 +14,11 @@ from explorepy.dashboard.dashboard import Dashboard
 
 __version__ = '1.3.0'
 _bt_interface = 'pybluez'
+
 if not sys.version_info >= (3, 6):
     raise EnvironmentError("Explorepy requires python versions 3.6 or newer!")
 logger.debug(f"Explorepy version: {__version__}")
-logger.debug(f"BT interface: {_bt_interface}")
+log_config.set_sentry_tag("explorepy.version", __version__)
 
 
 def set_bt_interface(bt_interface):
@@ -32,7 +32,7 @@ def set_bt_interface(bt_interface):
         raise ValueError("bt_interface must be either sdk or pybluez")
     import explorepy
     explorepy._bt_interface = bt_interface
-    logger.info(f"BT interface was changed to {bt_interface}")
+    logger.info(f"BT interface is set to {bt_interface}")
 
 
 def get_bt_interface():
