2d797c293c1862862ea75cd488fc0d68ab38957c
Atayi <35002731+bmeatayi@users.noreply.github.com>
2021-09-22 15:17:23 +0200

    V1.5.1 (#172)
    * update error log
    
    * fix bug after KeyboardInterrupt in impedance mode
    
    * Bump version: 1.5.1 → 1.5.2

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ if not os.environ.get('READTHEDOCS'):
         os.system('cp  lib/mac/exploresdk.py  src/explorepy')
 setup(
     name='explorepy',
-    version='1.5.1',
+    version='1.5.2',
     license='MIT license',
     description='Python API for Mentalab biosignal aquisition devices',
     long_description='%s\n%s' % (
