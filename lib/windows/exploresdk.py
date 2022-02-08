0ef8bca0074553fa2cf86204b428082dd293db4a
salman2135 <salman2135@gmail.com>
2022-02-08 15:04:15 +0100

    Revert "Revert "added SystemError for no bluetooth conenction for windows from SDK""
    This reverts commit 1ac687a46d85512eccad9da7d969d344199affdf.

--- lib/windows/exploresdk.py
+++ lib/windows/exploresdk.py
@@ -304,6 +304,20 @@ class ExploreIOException(object):
 # Register ExploreIOException in _exploresdk:
 _exploresdk.ExploreIOException_swigregister(ExploreIOException)
 
+class ExploreNoBluetoothException(object):
+    thisown = property(lambda x: x.this.own(), lambda x, v: x.this.own(v), doc="The membership flag")
+    __repr__ = _swig_repr
+
+    def __init__(self, message: "std::string"):
+        _exploresdk.ExploreNoBluetoothException_swiginit(self, _exploresdk.new_ExploreNoBluetoothException(message))
+
+    def what(self) -> "char const *":
+        return _exploresdk.ExploreNoBluetoothException_what(self)
+    __swig_destroy__ = _exploresdk.delete_ExploreNoBluetoothException
+
+# Register ExploreNoBluetoothException in _exploresdk:
+_exploresdk.ExploreNoBluetoothException_swigregister(ExploreNoBluetoothException)
+
 class device(object):
     thisown = property(lambda x: x.this.own(), lambda x, v: x.this.own(v), doc="The membership flag")
     __repr__ = _swig_repr
