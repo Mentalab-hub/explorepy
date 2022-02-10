c0f5738a9c090c9d05fed350006cfabe8e643765
salman <salman2135@gmail.com>
2022-02-10 11:55:11 +0100

    added dummy exceptions for mac OSX

--- lib/mac/ExploreException.h
+++ lib/mac/ExploreException.h
@@ -65,3 +65,23 @@ public:
 
 };
 
+
+// exception for unresponsive socket error
+class ExploreBtSocketException : std::exception
+{
+private:
+	std::string message;
+public:
+	ExploreBtSocketException(std::string message) NOEXCEPT
+	{
+		this->message = message;
+
+	}
+
+
+	virtual const char* what() const NOEXCEPT
+	{
+		return message.c_str();
+	}
+
+};
