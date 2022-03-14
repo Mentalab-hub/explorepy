16293f32e99b7e3056583fed5bc98f725ee12ff8
bmeatayi <bmeatayi@gmail.com>
2022-03-14 17:30:08 +0100

    test a failing test!

--- tests/test_explorepy.py
+++ tests/test_explorepy.py
@@ -6,7 +6,7 @@ explorepy.set_bt_interface('sdk')
 
 class TestMainClass:
     def test_dummy_1(self):
-        assert 1 == 1
+        assert 1 == 2
 
     def test_dummy_2(self):
         assert 2 == 2
