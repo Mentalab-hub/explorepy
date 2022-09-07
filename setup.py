35529910fcb26580ac81aa339d27645df1888010
salman2135 <salman2135@gmail.com>
2022-09-07 22:07:18 +0200

    bumpversion to 1.6.2

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ if not os.environ.get('READTHEDOCS'):
         os.system('cp  lib/mac/exploresdk.py  src/explorepy')
 setup(
     name='explorepy',
-    version='1.6.1',
+    version='1.6.2',
     license='MIT license',
     description='Python API for Mentalab biosignal aquisition devices',
     long_description_content_type="text/markdown",
