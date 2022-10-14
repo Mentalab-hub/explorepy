a6193f8f1bde1c78bb6373a00f62ba66b4107ffa
salman <salman2135@gmail.com>
2022-10-14 13:49:15 +0200

    add comment

--- src/explorepy/test.py
+++ src/explorepy/test.py
@@ -3,6 +3,7 @@ import time
 myexplore = Explore()
 myexplore.connect(device_name="Explore_1C3B")
 myexplore.record_data(file_name='test', duration=10, file_type='csv', do_overwrite= True)
+#wait until recording is done
 time.sleep(12)
 
 data_last = myexplore.get_last_recorded_data()
