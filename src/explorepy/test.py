61a98c6b23220ac23af56d1068ff8fbbdd3b36a2
Salman Rahman <salman2135@gmail.com>
2022-10-14 13:52:34 +0200

    Add comment with key values

--- src/explorepy/test.py
+++ src/explorepy/test.py
@@ -5,6 +5,6 @@ myexplore.connect(device_name="Explore_1C3B")
 myexplore.record_data(file_name='test', duration=10, file_type='csv', do_overwrite= True)
 #wait until recording is done
 time.sleep(12)
-
 data_last = myexplore.get_last_recorded_data()
-print("")
+
+# now it is possible to access exg and orientation by keys: data_last['exg'] and data_last['marker'] will return separate lists
