c919bf853d697bd108cbce343c97175b54289b4d
Salman Rahman <salman2135@gmail.com>
2022-10-20 12:07:47 +0200

    Update packet.py

--- src/explorepy/packet.py
+++ src/explorepy/packet.py
@@ -2,17 +2,13 @@
 """This module contains all packet classes of Mentalab Explore device"""
 import abc
 import logging
-import struct
 from enum import IntEnum
 
 import numpy as np
 
 from explorepy._exceptions import FletcherError
 
-
 logger = logging.getLogger(__name__)
-
-TIMESTAMP_SCALE = 10000
 
 
 class PACKET_ID(IntEnum):
@@ -24,16 +20,16 @@ class PACKET_ID(IntEnum):
     INFO = 99
     EEG94 = 144
     EEG98 = 146
+    EEG98_USBC = 150
     EEG99S = 30
     EEG99 = 62
     EEG94R = 208
     EEG98R = 210
     CMDRCV = 192
     CMDSTAT = 193
-    PUSHMARKER = 194
+    MARKER = 194
     CALIBINFO = 195
-    TRIGGER_OUT = 177  # Trigger-out of Explore device
-    TRIGGER_IN = 178   # Trigger-in to Explore device
+    CalibrationInfoQ = 197
 
 
 EXG_UNIT = 1e-6
@@ -43,16 +39,13 @@ class Packet:
     """An abstract base class for Explore packet"""
     __metadata__ = abc.ABCMeta
 
-    def __init__(self, timestamp, payload, time_offset=0):
+    def __init__(self, timestamp, payload):
         """Gets the timestamp and payload and initializes the packet object
 
         Args:
-            timestamp (double): Raw timestamp of the packet
-            payload (bytearray): A byte array including binary data and fletcher
-            time_offset (double): Time offset defined by parser. It will be the timestamp of the first packet when
-                                    streaming in realtime. It will be zero while converting a binary file.
+            payload (bytearray): a byte array including binary data and fletcher
         """
-        self.timestamp = timestamp / TIMESTAMP_SCALE + time_offset
+        self.timestamp = timestamp
 
     @abc.abstractmethod
     def _convert(self, bin_data):
@@ -86,11 +79,6 @@ class EEG(Packet):
     """EEG packet class"""
     __metadata__ = abc.ABCMeta
 
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
-        self.data = None
-        self.imp_data = None
-
     def calculate_impedance(self, imp_calib_info):
         """calculate impedance with the help of impedance calibration info
 
@@ -98,12 +86,8 @@ class EEG(Packet):
             imp_calib_info (dict): dictionary of impedance calibration info including slope, offset and noise level
 
         """
-        scale = imp_calib_info['slope']
-        offset = imp_calib_info['offset']
-        self.imp_data = np.round(
-            (self.get_ptp() - imp_calib_info['noise_level']) * scale / 1.e6 - offset,
-            decimals=0
-        )
+        self.imp_data = np.round((self.get_ptp() - imp_calib_info['noise_level']) * imp_calib_info['slope'] / 1.e6 -
+                                 imp_calib_info['offset'], decimals=0)
 
     def get_data(self, exg_fs=None):
         """get time vector and data
@@ -125,20 +109,12 @@ class EEG(Packet):
         """Get peak to peak value"""
         return np.ptp(self.data, axis=1)
 
-    def __str__(self):
-        pass
-
-    def _check_fletcher(self, fletcher):
-        pass
-
-    def _convert(self, bin_data):
-        pass
-
 
 class EEG94(EEG):
     """EEG packet for 4 channel device"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -162,8 +138,9 @@ class EEG94(EEG):
 
 class EEG98(EEG):
     """EEG packet for 8 channel device"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -187,8 +164,9 @@ class EEG98(EEG):
 
 class EEG99s(EEG):
     """EEG packet for 8 channel device"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -212,8 +190,9 @@ class EEG99s(EEG):
 
 class EEG99(EEG):
     """EEG packet for 8 channel device"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -234,14 +213,38 @@ class EEG99(EEG):
         return "EEG: " + str(self.data[:, -1])
 
 
-class Orientation(Packet):
-    """Orientation data packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+class EEG98_USBC(EEG):
+    """EEG packet for 8 channel devices"""
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
-        self.theta = None
-        self.rot_axis = None
+
+    def _convert(self, bin_data):
+        data = Packet.int24to32(bin_data)
+        n_chan = -1
+        v_ref = 4.0
+        n_packet = 16
+        data = data.reshape((n_packet, n_chan)).astype(np.float).T
+        gain = EXG_UNIT * ((2 ** 23) - 1) * 6.
+        self.data = np.round(data * v_ref / gain, 2)
+
+    def _check_fletcher(self, fletcher):
+        if not fletcher == b'\xaf\xbe\xad\xde':
+            raise FletcherError('Fletcher value is incorrect!')
+
+    def __str__(self):
+        return "EEG: " + str(self.data[:, -1])
+
+
+class Orientation(Packet):
+    """Orientation data packet"""
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
+        self._convert(payload[:-4])
+        self._check_fletcher(payload[-4:])
 
     def _convert(self, bin_data):
         data = np.copy(np.frombuffer(bin_data, dtype=np.dtype(np.int16).newbyteorder('<'))).astype(np.float)
@@ -277,8 +280,9 @@ class Orientation(Packet):
 
 class Environment(Packet):
     """Environment data packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -330,8 +334,9 @@ class Environment(Packet):
 
 class TimeStamp(Packet):
     """Time stamp data packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
         self.raw_data = None
@@ -348,124 +353,35 @@ class TimeStamp(Packet):
 
 
 class EventMarker(Packet):
-    """Abstract class for event markers"""
-    __metadata__ = abc.ABCMeta
+    """Marker packet"""
 
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
-        self.code = None
-        self._label_prefix = None
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
+        self._convert(payload[:-4])
+        self._check_fletcher(payload[-4:])
 
-    @abc.abstractmethod
     def _convert(self, bin_data):
-        pass
+        self.marker_code = np.frombuffer(bin_data, dtype=np.dtype(np.uint16).newbyteorder('<'))[0]
 
     def _check_fletcher(self, fletcher):
         if not fletcher == b'\xaf\xbe\xad\xde':
             raise FletcherError('Fletcher value is incorrect!')
 
+    def __str__(self):
+        return "Event marker: " + str(self.marker_code)
+
     def get_data(self, srate=None):
         """Get marker data
         Args:
             srate: NOT USED. Only for compatibility purpose"""
-        return [self.timestamp], [self._label_prefix + str(self.code)]
-
-    def __str__(self):
-        return f"{self.__class__.__name__}, Timestamp: {self.timestamp}, Code: {self.code}"
-
-
-class PushButtonMarker(EventMarker):
-    """Push Button Marker packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
-        self._convert(payload[:-4])
-        self._check_fletcher(payload[-4:])
-        self._label_prefix = 'pb_'
-
-    def _convert(self, bin_data):
-        self.code = np.frombuffer(bin_data, dtype=np.dtype(np.uint16).newbyteorder('<'))[0]
-
-
-class SoftwareMarker(EventMarker):
-    """Software marker packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
-        self._convert(payload[:-4])
-        self._check_fletcher(payload[-4:])
-        self._label_prefix = 'sw_'
-
-    def _convert(self, bin_data):
-        self.code = np.frombuffer(bin_data, dtype=np.dtype(np.uint16).newbyteorder('<'))[0]
-
-    @staticmethod
-    def create(local_time, code):
-        """ Create a software marker
-
-        Args:
-            local_time (double): Local time from LSL
-            code (int): Event marker code
-
-        Returns:
-            SoftwareMarker
-        """
-        return SoftwareMarker(local_time * TIMESTAMP_SCALE,
-                              payload=bytearray(struct.pack('<H', code) + b'\xaf\xbe\xad\xde')
-                              )
-
-
-class TriggerIn(EventMarker):
-    """Trigger in packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super(TriggerIn, self).__init__(timestamp, payload, time_offset)
-        self._time_offset = time_offset
-        self._convert(payload[:-4])
-        self._check_fletcher(payload[-4:])
-        self._label_prefix = 'in_'
-
-    def _convert(self, bin_data):
-        precise_ts = np.asscalar(np.frombuffer(bin_data,
-                                               dtype=np.dtype(np.uint32).newbyteorder('<'),
-                                               count=1,
-                                               offset=0))
-        self.timestamp = precise_ts / TIMESTAMP_SCALE + self._time_offset
-        code = np.asscalar(np.frombuffer(bin_data, dtype=np.dtype(np.uint16).newbyteorder('<'), count=1, offset=4))
-        self.code = code
-        mac_address = hex(int(np.frombuffer(bin_data, dtype=np.dtype(np.uint16).newbyteorder('<'), count=1, offset=6)))
-        self.mac_address = mac_address
-
-
-class TriggerOut(EventMarker):
-    """Trigger-out packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super(TriggerOut, self).__init__(timestamp, payload, time_offset)
-        self._time_offset = time_offset
-        self._convert(payload[:-4])
-        self._check_fletcher(payload[-4:])
-        self._label_prefix = 'out_'
-
-    def _convert(self, bin_data):
-        precise_ts = np.asscalar(np.frombuffer(bin_data,
-                                               dtype=np.dtype(np.uint32).newbyteorder('<'),
-                                               count=1,
-                                               offset=0))
-
-        self.timestamp = precise_ts / TIMESTAMP_SCALE + self._time_offset
-        code = np.asscalar(np.frombuffer(bin_data, dtype=np.dtype(np.uint16).newbyteorder('<'), count=1, offset=4))
-        """
-        if label == 240:
-            label = "Sync"
-        if label == 15:
-            label = "ADS_Start"
-        """
-        self.code = code
-        mac_address = hex(int(np.frombuffer(bin_data, dtype=np.dtype(np.uint16).newbyteorder('<'), count=1, offset=6)))
-        self.mac_address = mac_address
+        return [self.timestamp], [self.marker_code]
 
 
 class Disconnect(Packet):
     """Disconnect packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super().__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super().__init__(timestamp, payload)
         self._check_fletcher(payload)
 
     def _convert(self, bin_data):
@@ -481,8 +397,9 @@ class Disconnect(Packet):
 
 class DeviceInfo(Packet):
     """Device information packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super(DeviceInfo, self).__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super(DeviceInfo, self).__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -503,7 +420,7 @@ class DeviceInfo(Packet):
                     sampling_rate=self.sampling_rate)
 
     def __str__(self):
-        return "Firmware version: " + self.firmware_version + " - sampling rate: " + str(self.sampling_rate)\
+        return "Firmware version: " + self.firmware_version + " - sampling rate: " + str(self.sampling_rate) \
                + " Hz" + " - ADC mask: " + str(self.adc_mask)
 
     def get_data(self):
@@ -513,8 +430,9 @@ class DeviceInfo(Packet):
 
 class CommandRCV(Packet):
     """Command Status packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super(CommandRCV, self).__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super(CommandRCV, self).__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -531,8 +449,9 @@ class CommandRCV(Packet):
 
 class CommandStatus(Packet):
     """Command Status packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super(CommandStatus, self).__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super(CommandStatus, self).__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -550,8 +469,9 @@ class CommandStatus(Packet):
 
 class CalibrationInfo(Packet):
     """Calibration Info packet"""
-    def __init__(self, timestamp, payload, time_offset=0):
-        super(CalibrationInfo, self).__init__(timestamp, payload, time_offset)
+
+    def __init__(self, timestamp, payload):
+        super(CalibrationInfo, self).__init__(timestamp, payload)
         self._convert(payload[:-4])
         self._check_fletcher(payload[-4:])
 
@@ -574,6 +494,15 @@ class CalibrationInfo(Packet):
         return "calibration info: slope = " + str(self.slope) + "\toffset = " + str(self.offset)
 
 
+class CalibrationInfoQ(CalibrationInfo):
+    def __init__(self, timestamp, payload):
+        super(CalibrationInfoQ, self).__init__(timestamp, payload)
+
+    def _convert(self, bin_data):
+        super(CalibrationInfoQ, self)._convert()
+        self.offset = self.offset * 10
+
+
 PACKET_CLASS_DICT = {
     PACKET_ID.ORN: Orientation,
     PACKET_ID.ENV: Environment,
@@ -582,14 +511,14 @@ PACKET_CLASS_DICT = {
     PACKET_ID.INFO: DeviceInfo,
     PACKET_ID.EEG94: EEG94,
     PACKET_ID.EEG98: EEG98,
+    PACKET_ID.EEG98_USBC: EEG98_USBC,
     PACKET_ID.EEG99S: EEG99s,
     PACKET_ID.EEG99: EEG99s,
     PACKET_ID.EEG94R: EEG94,
     PACKET_ID.EEG98R: EEG98,
     PACKET_ID.CMDRCV: CommandRCV,
     PACKET_ID.CMDSTAT: CommandStatus,
-    PACKET_ID.CALIBINFO: CalibrationInfo,
-    PACKET_ID.PUSHMARKER: PushButtonMarker,
-    PACKET_ID.TRIGGER_IN: TriggerIn,
-    PACKET_ID.TRIGGER_OUT: TriggerOut
+    PACKET_ID.CALIBINFO: CalibrationInfoQ,
+    PACKET_ID.CALIBINFO: CalibrationInfoQ,
+    PACKET_ID.MARKER: EventMarker
 }
