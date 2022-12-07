b53e9326af989799b51a6bb4ea8011c403487256
Salman Rahman <salman2135@gmail.com>
2022-12-07 15:54:04 +0100

    add time vector data in record

--- src/explorepy/tools.py
+++ src/explorepy/tools.py
@@ -518,8 +518,10 @@ class FileRecorder:
                     self._write_edf_anno()
                     self._data = self._data[:, self._fs:]
         elif self.file_type == 'csv':
-            if isinstance(packet, EEG) and len(self.adc_mask) == 32:
-                data = data[[i for i, flag in enumerate(reversed(self.adc_mask)) if flag == 1]]
+            if isinstance(packet, EEG):
+                indices = [0] + [i + 1 for i, flag in enumerate(reversed(self.adc_mask)) if flag == 1]
+                print("indices are {}".format(indices))
+                data = data[indices]
             self._csv_obj.writerows(data.T.tolist())
             self._file_obj.flush()
 
