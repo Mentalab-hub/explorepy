838e78e9a70a34060f41501dae9608219c6244f2
Sonja Stefani <sonja.stefani@tum.de>
2023-02-01 12:52:33 +0100

    Converted unittest functions to use pytest and pytest-mock

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -1,10 +1,4 @@
-# import sys
-# import time
-# import pytest
-import unittest
-from unittest import TestCase
-from unittest.mock import patch
-
+import pytest
 import numpy as np
 
 from explorepy.packet import (
@@ -19,19 +13,18 @@ from explorepy.packet import (
 )
 
 
-class TestBasePacket(TestCase):
+class TestBasePacket:
 
     def test_is_abstract(self):
-        """
-        Test if explorepy.packet.Packet is abstract
-        """
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             Packet(12345, b'\x00\x00\x00\x00')
 
-    @patch.multiple(Packet, __abstractmethods__=set())
-    def test_init_timestamp_correct(self):
+    def test_init_timestamp_correct(self, mocker):
+        if hasattr(Packet, "__abstractmethods__"):
+            if len(Packet.__abstractmethods__) != 0:
+                mocker.patch.multiple(Packet, __abstractmethods__=set())
         p = Packet(12345, b'\x00\x00\x00\x00', 300)
-        self.assertEqual(p.timestamp, 301.2345)
+        assert p.timestamp == 301.2345
 
     def test_int24to32(self):
         res = Packet.int24to32([10, 20, 30, 40, 50, 60])
@@ -52,63 +45,65 @@ class TestBasePacket(TestCase):
         np.testing.assert_array_equal(res, [-8388608, -8388608])
 
 
-class TestEEGPacket(TestCase):
+class TestEEGPacket:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = EEG(12345, b'\xaf\xbe\xad\xde')
-        cls.p.data = np.array(
+    @pytest.fixture(autouse=True)
+    def setup_eeg(self, mocker):
+        if hasattr(EEG, "__abstractmethods__"):
+            if len(EEG.__abstractmethods__) != 0:
+                mocker.patch.multiple(EEG, __abstractmethods__=set())
+        self.eeg = EEG(12345, b'\xaf\xbe\xad\xde')
+        self.eeg.data = np.array(
             [[40, 3333, 78910, -30, 0], [20, -1000, 10, 30, 0], [10, 2345, 77016, 11, 45], [15, 1234, 70000, 2, 44]])
 
-    # Technically redundant because setUpClass will raise an exception if EEG isn't abstract
     def test_is_abstract(self):
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             EEG(12345, b'\x00\x00\x00\x00')
 
     def test_calculate_impedance_no_data(self):
         imp_calib_info = {'slope': None, 'offset': None, 'noise_level': None}
-        with self.assertRaises(Exception):
-            self.p.calculate_impedance(imp_calib_info)
+        with pytest.raises(Exception):
+            self.eeg.calculate_impedance(imp_calib_info)
 
     def test_get_data(self):
-        tv, d = self.p.get_data(250)
-        self.assertEqual(len(tv), 4)
+        tv, d = self.eeg.get_data(250)
+        assert len(tv) == 4
         np.testing.assert_array_equal(tv, [1.2345, 1.2385, 1.2425, 1.2465])
-        np.testing.assert_array_equal(d, self.p.data)
+        np.testing.assert_array_equal(d, self.eeg.data)
 
     def test_get_data_no_sample_rate(self):
-        tv, d = self.p.get_data()
-        self.assertEqual(tv, 1.2345)
-        np.testing.assert_array_equal(d, self.p.data)
+        tv, d = self.eeg.get_data()
+        assert tv == 1.2345
+        np.testing.assert_array_equal(d, self.eeg.data)
 
     def test_ptp(self):
-        res = self.p.get_ptp()
+        res = self.eeg.get_ptp()
         np.testing.assert_array_equal(res, [78940, 1030, 77006, 69998])
 
     def test_ptp_no_data(self):
-        self.p.data = b'\xaf\xbe\xad\xde'
-        with self.assertRaises(Exception):
-            self.p.get_ptp()
+        self.eeg.data = b'\xaf\xbe\xad\xde'
+        with pytest.raises(Exception):
+            self.eeg.get_ptp()
 
 
 # The data part of the packet has 507 - 12 bytes of data, with 3 bytes per channel, 4 channels, 1 status msg
 # with 4 channels + 1 status message @ 3 bytes each being 15 bytes, so (507-12)/(5*3) = 33 rows of data
 # (I think)
 # Note that this does *not* test the proper fletcher implementation
-class TestEEG94Packet(TestCase):
+class TestEEG94Packet:
 
-    @classmethod
-    def setUpClass(cls):
+    @pytest.fixture(autouse=True)
+    def setup_eeg94(self):
         # 99 bytes of data + 4 bytes fletcher (0xDEADBEAF)
-        cls.data = bytes([i % 256 for i in range(495)])  # 495 bytes of dummy data, resembles 33 samples in 4+1 channels
-        cls.fletcher = b'\xaf\xbe\xad\xde'
-        cls.eeg94 = EEG94(12345, cls.data + cls.fletcher, 300)
+        self.data = bytes([i % 256 for i in range(495)])  # 495 bytes of dummy data, resembles 33 samples in 4+1 channels
+        self.fletcher = b'\xaf\xbe\xad\xde'
+        self.eeg94 = EEG94(12345, self.data + self.fletcher, 300)
 
     # EEG94 converts the data on initialization,
     # if the packet size is fixed only a specific length of bytes should be accepted
     def test_data_length_too_long(self):
         payload = 2 * self.data + self.fletcher
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             EEG94(12345, payload, 300)
 
     def test_convert(self):
@@ -121,16 +116,17 @@ class TestEEG94Packet(TestCase):
     # TODO implement status message test
 
 
-class TestEEG98Packet(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.data = bytes([i % 256 for i in range(432)])
-        cls.fletcher = b'\xaf\xbe\xad\xde'
-        cls.eeg98 = EEG98(12345, cls.data + cls.fletcher, 300)
+class TestEEG98Packet:
+
+    @pytest.fixture(autouse=True)
+    def setup_eeg98(self):
+        self.data = bytes([i % 256 for i in range(432)])
+        self.fletcher = b'\xaf\xbe\xad\xde'
+        self.eeg98 = EEG98(12345, self.data + self.fletcher, 300)
 
     def test_data_too_long(self):
         payload = 2 * self.data + self.fletcher
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             EEG98(12345, payload, 300)
 
     def test_convert(self):
@@ -147,16 +143,17 @@ class TestEEG98Packet(TestCase):
     # TODO implement status message test
 
 
-class TestEEG99Packet(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.data = bytes([i % 256 for i in range(432)])
-        cls.fletcher = b'\xaf\xbe\xad\xde'
-        cls.eeg99 = EEG99(12345, cls.data + cls.fletcher, 300)
+class TestEEG99Packet:
+
+    @pytest.fixture(autouse=True)
+    def setup_eeg99(self):
+        self.data = bytes([i % 256 for i in range(432)])
+        self.fletcher = b'\xaf\xbe\xad\xde'
+        self.eeg99 = EEG99(12345, self.data + self.fletcher, 300)
 
     def test_data_too_long(self):
         payload = 2 * self.data + self.fletcher
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             EEG99(12345, payload, 300)
 
     def test_convert(self):
@@ -174,16 +171,17 @@ class TestEEG99Packet(TestCase):
     # TODO implement status message check
 
 
-class TestEEG99sPacket(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.data = bytes([i % 256 for i in range(432)])
-        cls.fletcher = b'\xaf\xbe\xad\xde'
-        cls.eeg99s = EEG99s(12345, cls.data + cls.fletcher, 300)
+class TestEEG99sPacket:
+
+    @pytest.fixture(autouse=True)
+    def setup_eeg99s(self):
+        self.data = bytes([i % 256 for i in range(432)])
+        self.fletcher = b'\xaf\xbe\xad\xde'
+        self.eeg99s = EEG99s(12345, self.data + self.fletcher, 300)
 
     def test_data_too_long(self):
         payload = 2 * self.data + self.fletcher
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             EEG99s(12345, payload, 300)
 
     def test_convert(self):
@@ -201,24 +199,29 @@ class TestEEG99sPacket(TestCase):
     # TODO implement status message check
 
 
-class TestOrientationPacket(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.data = bytes([i % 256 for i in range(432)])
-        cls.fletcher = b'\xaf\xbe\xad\xde'
-        cls.orientation = Orientation(12345, cls.data + cls.fletcher, 300)
+class TestOrientationPacket:
 
+    @pytest.fixture(autouse=True)
+    def setup_orientation(self):
+        self.data = bytes([i % 256 for i in range(432)])
+        self.fletcher = b'\xaf\xbe\xad\xde'
+        self.orientation = Orientation(12345, self.data + self.fletcher, 300)
+
+    @pytest.mark.skip(reason="Not implemented")
     def test_convert(self):
         self.fail()
 
+    @pytest.mark.skip(reason="Not implemented")
     def test_get_data(self):
         self.fail()
 
+    @pytest.mark.skip(reason="Not implemented")
     def test_compute_angle(self):
         self.fail()
 
 
-class TestEnvironmentPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestEnvironmentPacket:
     def test_convert(self):
         self.fail()
 
@@ -229,23 +232,26 @@ class TestEnvironmentPacket(TestCase):
         self.fail()
 
 
-class TestTimestampPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestTimestampPacket:
     # why does timestamp have a raw data var?
 
     def test_convert(self):
         self.fail()
 
 
-class TestEventMarkerPacket(TestCase):
+class TestEventMarkerPacket:
     def test_is_abstract(self):
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             EventMarker(12345, b'\xaf\xbe\xad\xde')
 
+    @pytest.mark.skip(reason="Not implemented")
     def test_get_data(self):
         self.fail()
 
 
-class TestPushButtonMarkerPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestPushButtonMarkerPacket:
     def test_convert(self):
         self.fail()
 
@@ -253,7 +259,8 @@ class TestPushButtonMarkerPacket(TestCase):
         self.fail()
 
 
-class TestSoftwareMarkerPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestSoftwareMarkerPacket:
     def test_convert(self):
         self.fail()
 
@@ -265,7 +272,8 @@ class TestSoftwareMarkerPacket(TestCase):
         self.fail()
 
 
-class TestTriggerInPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestTriggerInPacket:
     def test_prefix(self):
         self.fail()
 
@@ -273,7 +281,8 @@ class TestTriggerInPacket(TestCase):
         self.fail()
 
 
-class TestTriggerOutPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestTriggerOutPacket:
     def test_prefix(self):
         self.fail()
 
@@ -281,12 +290,14 @@ class TestTriggerOutPacket(TestCase):
         self.fail()
 
 
-class TestDisconnectPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestDisconnectPacket:
     def test_convert(self):
         self.fail()
 
 
-class TestDeviceInfoPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestDeviceInfoPacket:
     def test_convert(self):
         self.fail()
 
@@ -297,23 +308,22 @@ class TestDeviceInfoPacket(TestCase):
         self.fail()
 
 
-class TestCommandRCVPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestCommandRCVPacket:
     def test_convert(self):
         self.fail()
 
 
-class TestCommandStatusPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestCommandStatusPacket:
     def test_convert(self):
         self.fail()
 
 
-class TestCalibrationInfoPacket(TestCase):
+@pytest.mark.skip(reason="Not implemented")
+class TestCalibrationInfoPacket:
     def test_convert(self):
         self.fail()
 
     def test_get_info(self):
         self.fail()
-
-
-if __name__ == '__main__':
-    unittest.main()
