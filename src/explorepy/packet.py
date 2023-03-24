55eaf31f8eb1c6b208db4fae2a3e6d9e8ef36d1f
Salman Rahman <salman2135@gmail.com>
2023-03-24 15:27:36 +0100

    update trigger in and out packets (#258)
    * update trigger in and out packets
    
    * undo debug change

--- src/explorepy/packet.py
+++ src/explorepy/packet.py
@@ -556,13 +556,13 @@ class TriggerIn(EventMarker):
         self._label_prefix = "in_"
 
     def _convert(self, bin_data):
-        precise_ts = np.asscalar(
+        precise_ts = np.ndarray.item(
             np.frombuffer(bin_data,
                           dtype=np.dtype(np.uint32).newbyteorder("<"),
                           count=1,
                           offset=0))
         self.timestamp = precise_ts / TIMESTAMP_SCALE + self._time_offset
-        code = np.asscalar(
+        code = np.ndarray.item(
             np.frombuffer(bin_data,
                           dtype=np.dtype(np.uint16).newbyteorder("<"),
                           count=1,
@@ -590,14 +590,14 @@ class TriggerOut(EventMarker):
         self._label_prefix = "out_"
 
     def _convert(self, bin_data):
-        precise_ts = np.asscalar(
+        precise_ts = np.ndarray.item(
             np.frombuffer(bin_data,
                           dtype=np.dtype(np.uint32).newbyteorder("<"),
                           count=1,
                           offset=0))
 
         self.timestamp = precise_ts / TIMESTAMP_SCALE + self._time_offset
-        code = np.asscalar(
+        code = np.ndarray.item(
             np.frombuffer(bin_data,
                           dtype=np.dtype(np.uint16).newbyteorder("<"),
                           count=1,
