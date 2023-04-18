57fd2c74282fceb694776fb764ef16f39e522bef
salman <salman2135@gmail.com>
2023-04-18 16:39:16 +0200

    mock client and server class with connect, reconenct and disconnect

---
+++ src/explorepy/bt_mock_server.py
@@ -0,0 +1,13 @@
+class MockBtServer:
+
+    def __init__(self):
+        self.is_connected = False
+
+    def Connect(self):
+        self.is_connected = True
+        return 0
+
+    def Close(self):
+        self.is_connected = False
+
+
