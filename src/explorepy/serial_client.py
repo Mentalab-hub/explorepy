16d9243f1f119ee8cf54c65f4f21abe71d72992b
Salman Rahman <salman2135@gmail.com>
2024-12-19 13:16:35 +0100

    check device name before usb connection

--- src/explorepy/serial_client.py
+++ src/explorepy/serial_client.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """A module for bluetooth connection"""
 import logging
+import struct
 import threading
 import time
-from queue import Queue
+
 import serial
 from serial.tools import list_ports
-import binascii
 
 from explorepy import (
     exploresdk,
@@ -101,7 +101,7 @@ class SerialClient:
         except Exception as error:
             print(error)
             logger.error(
-                "unknown error occured while reading bluetooth data by "
+                "unknown error occurred while reading bluetooth data by "
                 "pyserial {} of type:{}".format(error, type(error))
             )
 
@@ -118,6 +118,23 @@ class SerialClient:
         return (device_name[-4:-2] == mac_address[-5:-3]) and (device_name[-2:] == mac_address[-2:])
 
 
+def get_device_name(p):
+    """ Gets name of the Explore device
+    Args:
+        p (port instance): number of bytes to be read
+    """
+    serial_port = serial.Serial(port=p.device, baudrate=115200, timeout=2)
+    get_name_cmd = b'\xC6' * 14
+    serial_port.write(get_name_cmd)
+    data = serial_port.read(4)
+    length = struct.unpack('<H', data[2:])[0]
+    data = serial_port.read(length)
+    name = data[4:-4].decode('utf-8', errors='ignore')
+    serial_port.close()
+    time.sleep(1)
+    return name
+
+
 class SerialStream:
     """ Responsible for Connecting and reconnecting explore devices via bluetooth"""
 
@@ -126,7 +143,7 @@ class SerialStream:
         """
         self.is_connected = False
         self.device_name = device_name
-        self.bt_serial_port_manager = None
+        self.comm_manager = None
         self.device_manager = None
         self.bt_sdk = None
         self.usb_stop_flag = threading.Event()
@@ -137,7 +154,7 @@ class SerialStream:
         """Reads data in fixed-size chunks from the serial port until stopped."""
         while not self.usb_stop_flag.is_set():
             try:
-                data = self.bt_serial_port_manager.read(2048)
+                data = self.comm_manager.read(2048)
                 if data is not None:
                     self.copy_buffer.extend(data)
             except Exception as e:
@@ -151,25 +168,19 @@ class SerialStream:
         Returns:
             socket (bluetooth.socket)
         """
-        ports = list(list_ports.comports())
-        port = 0
-        for p in ports:
-            if p.vid == 0x0483 and p.pid == 0x5740:
-                port = p.device
-        if port == 0:
-            logger.debug("Could not find any suitable explore device")
+        port = self.scan_usb_ports()
 
         for _ in range(5):
             try:
-                self.bt_serial_port_manager = serial.Serial(port=port, baudrate=115200, timeout=2)
+                self.comm_manager = serial.Serial(port=port, baudrate=115200, timeout=2)
 
                 # stop stream
                 cmd = b'\xE5' * 14
-                self.bt_serial_port_manager.write(cmd)
+                self.comm_manager.write(cmd)
                 time.sleep(1)
 
                 cmd = b'\xE4' * 14
-                self.bt_serial_port_manager.write(cmd)
+                self.comm_manager.write(cmd)
                 time.sleep(1)
 
                 self.reader_thread = threading.Thread(
@@ -184,8 +195,8 @@ class SerialStream:
                 pass
             except serial.serialutil.SerialException:
                 logger.info(
-                    'Permission denied on serial port access, please run this command via terminal: sudo chmod 777 {}'.format(
-                    port)
+                    'Permission denied on serial port access, please run this command via \
+                    terminal: sudo chmod 777 {}'.format(port)
                 )
             except Exception as error:
                 self.is_connected = False
@@ -198,13 +209,45 @@ class SerialStream:
 
         self.is_connected = False
         raise DeviceNotFoundError(
-            "Could not find the device! Please turn on the device, wait a few seconds and connect to serial port before starting ExplorePy"
+            "Could not find the device! Please turn on the device, wait a few seconds and connect to \
+            serial port before starting ExplorePy"
+        )
+
+    def scan_usb_ports(self):
+        ports = list(list_ports.comports())
+        for p in ports:
+            try:
+                if p.vid == 0x0483 and p.pid == 0x5740:
+                    # Check device name
+                    name = get_device_name(p)
+                    if name == self.device_name:
+                        logger.info('Device connected to USB port.')
+                        return p.device
+            except PermissionError:
+                # do nothing here as this comes from posix
+                pass
+            except serial.serialutil.SerialException:
+                logger.info(
+                    "Permission denied on serial port access, please run this command via terminal:\
+                    sudo chmod 777 {}".format(p)
+                )
+            except Exception as error:
+                self.is_connected = False
+                logger.info(
+                    "Got an exception while connecting to the device: {} of type: {}".format(error, type(error))
+                )
+                logger.debug('trying to connect again as tty port is not visible yet')
+                logger.warning("Could not connect; Retrying in 2s...")
+                time.sleep(2)
+        raise DeviceNotFoundError(
+            "Could not find the device! Please turn on the device,\
+            wait a few seconds and connect to serial port before starting ExplorePy"
         )
 
     def reconnect(self):
         """Reconnect to the last connected device
 
-        Generally speaking this is not necessary for USB stream but we keep it as placeholder
+        Generally speaking this is not necessary for USB stream, but we keep it as placeholder
         in case it is needed in future
         """
         raise NotImplementedError
@@ -214,8 +257,8 @@ class SerialStream:
         self.usb_stop_flag.set()
         self.reader_thread.join(timeout=2)
         self.is_connected = False
-        self.bt_serial_port_manager.cancel_read()
-        self.bt_serial_port_manager.close()
+        self.comm_manager.cancel_read()
+        self.comm_manager.close()
         time.sleep(1)
 
     def read(self, n_bytes):
@@ -229,10 +272,10 @@ class SerialStream:
         """
         try:
             count = 5
-            while len(self.copy_buffer) < n_bytes and count > 0 :
+            while len(self.copy_buffer) < n_bytes and count > 0:
                 time.sleep(.01)
                 count -= 1
-            data=  self.copy_buffer[:n_bytes]
+            data = self.copy_buffer[:n_bytes]
             self.copy_buffer = self.copy_buffer[n_bytes:]
             return data
         except Exception as error:
@@ -244,4 +287,4 @@ class SerialStream:
         Args:
             data (bytearray): Data to be sent
         """
-        self.bt_serial_port_manager.write(data)
+        self.comm_manager.write(data)
