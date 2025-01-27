72074a59977feb39b0a953287219ca189af8190d
Salman Rahman <salman2135@gmail.com>
2025-01-27 14:20:32 +0100

    Revert "simplify-mac-intel-connection"
    This reverts commit f2a1a86da4477eba8067c50958f516c1cd2b4206.

--- src/explorepy/serial_client.py
+++ src/explorepy/serial_client.py
@@ -42,11 +42,11 @@ class SerialClient:
         if self.mac_address is None:
             self._find_mac_address()
             config_manager.set_mac_address(self.mac_address)
-        #result = exploresdk.BTSerialPortBinding.Create(self.mac_address, 5).Connect()
-        #print('result is {}'.format(result))
+        result = exploresdk.BTSerialPortBinding.Create(self.mac_address, 5).Connect()
+        print('result is {}'.format(result))
         for _ in range(5):
             try:
-                self.bt_serial_port_manager = serial.Serial('/dev/tty.' + self.device_name, 9600, timeout=2)
+                self.bt_serial_port_manager = serial.Serial('/dev/tty.' + self.device_name, 9600, timeout=5)
                 print('/dev/tty.' + self.device_name)
                 self.is_connected = True
                 return 0
@@ -95,14 +95,8 @@ class SerialClient:
             Returns:
                 list of bytes
         """
-        c = 0
         try:
-            while c <= 5:
-                read_output = self.bt_serial_port_manager.read(n_bytes)
-                if len(read_output) < n_bytes:
-                    continue
-                else:
-                    break
+            read_output = self.bt_serial_port_manager.read(n_bytes)
             return read_output
         except Exception as error:
             print(error)
