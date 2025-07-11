5ec70249e606ba5b2ee212ad6b628ef6f4a2ac30
Salman Rahman <salman2135@gmail.com>
2025-07-11 16:21:48 +0200

    bumpversion minor

--- src/explorepy/__init__.py
+++ src/explorepy/__init__.py
@@ -20,7 +20,7 @@ from .explore import Explore  # noqa
 
 
 __all__ = ["Explore", "command", "tools", "log_config"]
-__version__ = '4.1.0'
+__version__ = '4.2.0'
 
 this = sys.modules[__name__]
 # TODO appropriate library
