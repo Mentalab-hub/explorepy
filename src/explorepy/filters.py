dcb7bbb66447575d21c48219643cfbfe8863f443
Salman Rahman <salman2135@gmail.com>
2026-02-24 15:31:54 +0100

    lower order for 1000 Hz filter

--- src/explorepy/filters.py
+++ src/explorepy/filters.py
@@ -31,7 +31,7 @@ class ExGFilter:
         self.s_rate = float(s_rate)
         self.filter_type = filter_type
         self.filter_param = None
-        order = 2 if self.s_rate > 1000 else order
+        order = 2 if self.s_rate >= 1000 else order
         a, b, zi = self.get_filter_coeffs(cutoff_freq, filter_type, s_rate, n_chan, order)
         self.filter_param = {'a': a, 'b': b, 'zi': zi}
 
