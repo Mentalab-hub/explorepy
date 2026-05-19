fded05ab978dfd40c23f9858a3f22ba0ef79b91d
Salman Rahman <salman2135@gmail.com>
2026-05-19 16:31:22 +0200

    Rc 4.5.0 (#435)
    * bumpversion minor
    
    * pip deps on toml
    
    * remove sps from args in imp recorder (#436)

--- src/explorepy/__init__.py
+++ src/explorepy/__init__.py
@@ -20,7 +20,7 @@ from .explore import Explore  # noqa
 
 
 __all__ = ["Explore", "command", "tools", "log_config"]
-__version__ = '4.4.0'
+__version__ = '4.5.0'
 
 this = sys.modules[__name__]
 # TODO appropriate library
