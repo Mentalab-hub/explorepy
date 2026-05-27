2e086cbef1420fa2b4bfe74d572948d6336edc00
Salman Rahman <salman2135@gmail.com>
2026-05-27 16:53:35 +0200

    Hotfix bdf recording (#444)
    * change depricated function
    
    * Flush EDF buffer at quarter-second intervals
    
    Reduce the threshold for writing EDF samples from _fs to _fs // 4 so buffered data is flushed more frequently. Previously the FileRecorder only wrote when the buffer exceeded one second of samples; this change triggers writes at roughly quarter-second increments to lower memory usage and latency while retaining the existing behavior of writing full _fs-sized chunks and updating annotations/timestamps under the same lock.

--- src/explorepy/tools.py
+++ src/explorepy/tools.py
@@ -362,7 +362,7 @@ class FileRecorder:
         self._data = np.concatenate((self._data, data), axis=1)
         self._timestamps += list(data[0, :])
         with lock:
-            if self._data.shape[1] > self._fs:
+            if self._data.shape[1] > self._fs // 4:
                 self._file_obj.writeSamples(list(self._data[:, :self._fs]))
                 self._write_edf_anno()
                 self._data = self._data[:, self._fs:]
